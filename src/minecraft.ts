5e0409b981024d6c91d28646908d2e5252756eff
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2022-10-10 22:04:17 +0600

    Adding query, ping, favicon, profile, UUID functions and removing Serverinfo

--- src/minecraft.ts
+++ src/minecraft.ts
@@ -1,15 +1,61 @@
-const request = require('request');
+const request = require("request");
 
 let url: any;
 
-const Serverinfo = (ip: string, port: number = 25565, callback: any) => {
-    url = `https://mcapi.us/server/status?ip${ip}&port=${port}`;
-    request(url, { json: true }, (err: any, res: any, body: any) => {
-        if(err) {
-            return callback(err);
-        }
-        return callback(body)
-    });
-}
+const query = (ip: string, port: number = 25565, callback: any) => {
+  url = `https://api.minetools.eu/query/${ip}/${port}`;
+  request(url, { json: true }, (err: any, res: any, body: any) => {
+    if (err) {
+      return callback(err);
+    }
+    return callback(body);
+  });
+};
 
-module.exports = Serverinfo;
+const UUID = (username: string, callback: any) => {
+  url = `https://api.minetools.eu/uuid/${username}`;
+  request(url, { json: true }, (err: any, res: any, body: any) => {
+    if (err) {
+      return callback(err);
+    }
+    return callback(body);
+  });
+};
+
+const profile = (uuid: string, callback: any) => {
+  url = `https://api.minetools.eu/profile/${uuid}`;
+  request(url, { json: true }, (err: any, res: any, body: any) => {
+    if (err) {
+      return callback(err);
+    }
+    return callback(body);
+  });
+};
+
+const favicon = (ip: string, port: number = 25565, callback: any) => {
+  url = `https://api.minetools.eu/favicon/${ip}/${port}`;
+  request(url, { json: false }, (err: any, res: any, body: any) => {
+    if (err) {
+      return callback(err);
+    }
+    return callback(body);
+  });
+};
+
+const ping = (ip: string, port: number = 25565, callback: any) => {
+  url = `https://api.minetools.eu/ping/${ip}/${port}`;
+  request(url, { json: true }, (err: any, res: any, body: any) => {
+    if (err) {
+      return callback(err);
+    }
+    return callback(body);
+  });
+};
+
+module.exports = {
+    query,
+    ping,
+    favicon,
+    UUID,
+    profile
+}
