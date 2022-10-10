a9c7bfe2cba8a03abcf222666ce485c74c0190ce
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2022-10-10 22:04:27 +0600

    exporting modules

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-exports = require('./minecraft');
+exports.query = require('./minecraft');
+exports.ping = require('./minecraft');
+exports.favicon = require('./minecraft');
+exports.UUID = require('./minecraft');
+exports.profile = require('./minecraft');
+
+module.exports = require('./minecraft');
